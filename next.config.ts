f00c760eb10b4ff1aa2bc512b43a0afa677f17b5
My Name <emailkamu@example.com>
2026-01-20 01:09:14 +0700

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "3mb",
+    },
+  },
+
   images: {
     remotePatterns: [
       {
